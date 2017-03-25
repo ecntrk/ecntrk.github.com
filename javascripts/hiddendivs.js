154029201cb50b26d8a8cebfe1398f0affc3c273
Debmalya Sinha <debmalya.01@gmail.com>
2017-03-25 00:01:30 +0000

    length updated

--- javascripts/hiddendivs.js
+++ javascripts/hiddendivs.js
@@ -1,12 +1,15 @@
 function toggleDesc(id) {
         var el;
         el = document.getElementById(id);
+        var txt = el.innerHTML.split('<',1);
 
+var txtl = Math.round((txt[0].length/80)*25 +40);
+txtl = txtl+ 'px';
         if(el.style.visibility==='hidden' || el.style.visibility==='')
         {          
           el.style.opacity = "1.0";
           el.style.visibility='visible';
-          el.style.height = '120px';
+          el.style.height = txtl;
         }
         else
         {
