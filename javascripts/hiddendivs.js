27eb74c39caf63e6f930c3ee5bb25d4c85265506
Debmalya Sinha <debmalya.01@gmail.com>
2020-06-30 00:55:33 +0100

    mobile screen debug

--- javascripts/hiddendivs.js
+++ javascripts/hiddendivs.js
@@ -1,11 +1,15 @@
 function toggleDesc(id) {
   el = document.getElementById(id);
-  el2 = document.getElementById(id+"_R");
+  el2 = document.getElementById(id+'_R');
 //  txLen = Math.max(document.getElementById(id+'_S').innerHTML.length, document.getElementById(id+'_TE').innerHTML.length);
   txLen =document.getElementById(id+'_S').innerHTML.length;
   var extraspace = "";
+  var el2Height = "20px";
   if(el.clientWidth < txLen *5.75) //magic number!
+  {
     extraspace="<br>";
+    el2Height = "40px";
+  }
 
   var txt = el.innerHTML.split('<',1);
   var txtl = Math.round((txt[0].length/80)*25 +40);
@@ -14,18 +18,20 @@ function toggleDesc(id) {
   txtl = txtl+ 'px';
 
   if(el.style.visibility==='hidden' || el.style.visibility==='')
-  {          
+  { 
+    el2.style.height = el2Height;         
+    el2.innerHTML =  extraspace+"less ▲";
     el.style.opacity = "1.0";
     el.style.visibility='visible';
     el.style.height = txtl;
-    el2.innerHTML =  extraspace+"less ▲"+txLen+"," +el.clientWidth
   }
   else
   {
+    el2.style.height = '20px';         
     el.style.opacity = "0.0";
     el.style.visibility='hidden';
     el.style.height = '1px';
-    el2.innerHTML = extraspace+"more ▼"+txLen+"," +el.clientWidth
+    el2.innerHTML = extraspace+"more ▼";
   }
 }
 
@@ -37,8 +43,12 @@ function toggleCur(id, choice) {
   txLen =document.getElementById(id+'_S').innerHTML.length;
 
   var extraspace = "";
+  var el2Height = "20px";
   if(el.clientWidth < txLen *5.75) //magic number!
-  extraspace="<br>";
+  {
+    extraspace="<br>";
+    el2Height = "40px";
+  }
 
   if(el2.style.visibility==='hidden'|| el2.style.visibility==='')
   {
@@ -46,7 +56,7 @@ function toggleCur(id, choice) {
   {          
     el.style.opacity = "1.0";
     el.style.visibility='visible';
-    el.style.height = '20px';
+    el.style.height = el2Height;
     el.innerHTML = extraspace+"more ▼"+txLen+"," +el.clientWidth ;
     
   }
